89cbc1ba82397a462972714e71774b18c7e934ed
jchoczewska <50578930+jchoczewska@users.noreply.github.com>
2020-04-30 12:07:53 +0200

    moved hamilton_cycle to graph.py

--- graph.py
+++ graph.py
@@ -595,10 +595,10 @@ def print_eulerian_path(input_graph):
 
 
 def random_graph_degree_count(
-    vertex_count,
-    degree,
-    graph_type=AdjacencyMatrix):
-    vertices = int(degree*vertex_count/2)
+        vertex_count,
+        degree,
+        graph_type=AdjacencyMatrix):
+    vertices = int(degree * vertex_count / 2)
     all_pairs = []
     for idx1 in range(vertex_count):
         for idx2 in range(idx1):
@@ -609,12 +609,12 @@ def random_graph_degree_count(
         table_of_degrees.append(degree)
 
     matrix = np.zeros((vertex_count, vertex_count), dtype=int)
-    v=vertices
-    i=0
-    j=0
+    v = vertices
+    i = 0
+    j = 0
     while i < vertices:
-        if(j%60==59):
-            i=0
+        if (j % 60 == 59):
+            i = 0
             table_of_degrees.clear()
             all_pairs.clear()
             for x in range(vertex_count):
@@ -623,23 +623,22 @@ def random_graph_degree_count(
                 for idx2 in range(idx1):
                     all_pairs.append((idx1, idx2))
             matrix = np.zeros((vertex_count, vertex_count), dtype=int)
-           
+
         random.shuffle(all_pairs)
-        #print(all_pairs)           POMAGAJĄ ZROZUMIEC DZIALANIE FUNKCJI
-        #print(table_of_degrees)    SKUTECZNOŚC 100% ALE TROCHĘ OSZUKANA
-        j=j+1
-        
-        if ((table_of_degrees[all_pairs[0][0]]>0) and (table_of_degrees[all_pairs[0][1]]>0)):
-            matrix[all_pairs[0][0]][all_pairs[0][1]] = 1+matrix[all_pairs[0][0]][all_pairs[0][1]]
-            matrix[all_pairs[0][1]][all_pairs[0][0]] = 1+matrix[all_pairs[0][1]][all_pairs[0][0]]
-            table_of_degrees[all_pairs[0][0]] = table_of_degrees[all_pairs[0][0]]-1 
-            table_of_degrees[all_pairs[0][1]] = table_of_degrees[all_pairs[0][1]]-1
+        # print(all_pairs)           POMAGAJĄ ZROZUMIEC DZIALANIE FUNKCJI
+        # print(table_of_degrees)    SKUTECZNOŚC 100% ALE TROCHĘ OSZUKANA
+        j = j + 1
+
+        if ((table_of_degrees[all_pairs[0][0]] > 0) and (table_of_degrees[all_pairs[0][1]] > 0)):
+            matrix[all_pairs[0][0]][all_pairs[0][1]] = 1 + matrix[all_pairs[0][0]][all_pairs[0][1]]
+            matrix[all_pairs[0][1]][all_pairs[0][0]] = 1 + matrix[all_pairs[0][1]][all_pairs[0][0]]
+            table_of_degrees[all_pairs[0][0]] = table_of_degrees[all_pairs[0][0]] - 1
+            table_of_degrees[all_pairs[0][1]] = table_of_degrees[all_pairs[0][1]] - 1
             all_pairs.remove(all_pairs[0])
-            i=i+1
-             
+            i = i + 1
+
         else:
             random.shuffle(all_pairs)
-                
 
     result = AdjacencyMatrix(matrix)
     return convert(result, graph_type)
@@ -649,15 +648,43 @@ def random_regular_graph(
         vertex_count,
         degree,
         graph_type=AdjacencyMatrix):
-
-    if degree*vertex_count%2==1:
+    if degree * vertex_count % 2 == 1:
         raise ValueError("degree and vertex count must be even")
     if not 0 <= degree < vertex_count:
         raise ValueError("0 <= degree < vertex count must be satisfied")
-               
+
     return random_graph_degree_count(
         vertex_count,
         degree,
         graph_type)
 
 
+# number of hamilton cycles found in a graph
+cycles_found = 0
+
+
+def hamilton_cycle(v, in_graph, n, stack, visited):
+    global cycles_found
+    stack = np.append(stack, v)
+
+    if stack.shape[0] < n:
+        visited[v - 1] = True
+        for neighbour in in_graph.neighbours_lists[v - 1]:
+            if not visited[neighbour]:
+                hamilton_cycle(neighbour + 1, in_graph, n, stack, visited)
+        visited[v - 1] = False
+    else:
+        test = False
+        for neighbour in in_graph.neighbours_lists[v - 1]:
+            if neighbour == 0:
+                test = True
+                break
+        if test:
+            cycles_found += 1
+            cycle = "["
+            for i in range(stack.shape[0]):
+                cycle += str(stack[i]) + " - "
+            cycle += str(stack[0]) + "]"
+            print(cycle)
+    stack = np.delete(stack, stack.shape[0] - 1)
+    return cycles_found
--- projekt2.py
+++ projekt2.py
@@ -1,33 +1,6 @@
 import graph as gp
 import numpy as np
 
-
-def hamilton_cycle(v, in_graph, n):
-    global stack, visited, test, cycles_found
-    stack = np.append(stack, v)
-
-    if stack.shape[0] < n:
-        visited[v - 1] = True
-        for neighbour in in_graph.neighbours_lists[v - 1]:
-            if not visited[neighbour]:
-                hamilton_cycle(neighbour + 1, in_graph, n)
-        visited[v - 1] = False
-    else:
-        test = False
-        for neighbour in in_graph.neighbours_lists[v - 1]:
-            if neighbour == 0:
-                test = True
-                break
-        if test:
-            cycles_found += 1
-            cycle = "["
-            for i in range(stack.shape[0]):
-                cycle += str(stack[i]) + " - "
-            cycle += str(stack[0]) + "]"
-            print(cycle)
-    stack = np.delete(stack, stack.shape[0] - 1)
-
-
 print("PROJEKCIK 2 GRAFY")
 
 # Ad1 Check if sequence
@@ -54,23 +27,19 @@ gp.draw_graph(eulerian_graph)
 gp.print_eulerian_path(eulerian_graph)
 
 # Ad5 random regular graph
-rnd_graph = gp.random_regular_graph(6,4)
+rnd_graph = gp.random_regular_graph(6, 4)
 gp.draw_graph(rnd_graph)
 
 # Ad6 Hamilton Cycle
 graph_in_file = gp.read_graph_from_file('projekt2/ham_ex2.txt')
 graph = gp.convert(graph_in_file, gp.AdjacencyList)
 
-# number of hamilton cycles found in a graph
-cycles_found = 0
 # array of visited vertices
 visited = np.zeros(graph.vertex_count, bool)
 # storage for hamilton cycle
 stack = np.array([], int)
-# test if graph is hamiltonian
-test = False
 print("SZUKANIE CYKLU HAMILTONA")
-hamilton_cycle(1, graph, graph.vertex_count)
+cycles_found = gp.hamilton_cycle(1, graph, graph.vertex_count, stack, visited)
 if cycles_found == 0:
     print("Nie znaleziono cyklu Hamiltona. Podany graf nie jest grafem hamiltonowskim.")
 else:
