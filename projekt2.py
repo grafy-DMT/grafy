8226809c6c36f1c60df0648313d491efe9ad06f1
Patryk Chodur <patrykchodur@icloud.com>
2020-04-30 11:13:28 +0200

    moved ex5 to graph.py

--- projekt2.py
+++ projekt2.py
@@ -30,14 +30,20 @@ def hamilton_cycle(v, in_graph, n):
 
 print("PROJEKCIK 2 GRAFY")
 
+# Ad1 Check if sequence
 sequence1 = [4, 2, 2, 3, 2, 1, 4, 2, 2, 2, 2]
 sequence2 = [4, 4, 3, 1, 2]
 
 print("Sequence1: " + str(gp.is_graph_seq(sequence1)))
 print("Sequence2: " + str(gp.is_graph_seq(sequence2)))
 
+# Ad1 Get graph from sequence
 graph_from_sequence1 = gp.graph_from_degree_seq(sequence1)
 gp.draw_graph(graph_from_sequence1)
+
+# Ad2 Randomize graph
+randomized_graph = gp.randomize_graph(graph_from_sequence1, 10)
+gp.draw_graph(randomized_graph)
 
 # Ad3 Components
 gp.print_components_and_max_component(graph_from_sequence1)
@@ -46,6 +52,10 @@ gp.print_components_and_max_component(graph_from_sequence1)
 eulerian_graph = gp.read_graph_from_file('projekt2/eulerian_graph.txt')
 gp.draw_graph(eulerian_graph)
 gp.print_eulerian_path(eulerian_graph)
+
+# Ad5 random regular graph
+rnd_graph = gp.random_regular_graph(6,4)
+gp.draw_graph(rnd_graph)
 
 # Ad6 Hamilton Cycle
 graph_in_file = gp.read_graph_from_file('projekt2/ham_ex2.txt')
